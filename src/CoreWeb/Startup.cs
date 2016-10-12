99deb5927e89e4f497a87ffbb432ef9d49e966c6
Dan Frank <dan.frank@hp.com>
2016-10-11 21:26:22 -0600

    Initial commit.

---
+++ src/CoreWeb/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Marten;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Arkdj.Chronicle.Web
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+
+            var pgHost = Environment.GetEnvironmentVariable("CHRONICLE_PGHOST");
+            Console.WriteLine($"Connecting to PGHOST: {pgHost}.");
+            services.AddSingleton<IDocumentStore>(provider =>
+                DocumentStore.For(_ =>
+                {
+                    _.Connection($"Server={pgHost};Port=5432;Database=svsim;User Id=chron;Password=chron;");
+                    _.DatabaseSchemaName = "chron";
+                }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+        }
+    }
+}
